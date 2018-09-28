18feef606d7f23d36cae5394d7eaaf0eab84b845
Eric Zheng <zheng.eric.y@gmail.com>
2018-09-27 20:02:10 -0400

    init documentation directory

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# mate2018/docs
+
+> technical documentation for MATE 2019
+
+This directory is the central development workspace for the Rogue Robotics technical documentation for the 2019 MATE ROV competition. The primary contents are:
+
+* `rogue2019.tex`: the main LaTeX source
+* `rogue2019-cover.pdf`: the pretty cover page
+* `rogue2019.pdf`: the compiled documentation
+* `img/`: the image resources we need
+* `build.sh`: the build script
+* `roguedoc.cls`: the LaTeX class file
+
+I'm still in the process of migrating the old 2018 resources here, so it's still incomplete.
---
+++ docs/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# set the file name
+FNAME='rogue2019'
+
+# this compiles the documentation
+echo 'BUILD.SH: welcome! i will dutifully compile your documentation'
+echo '          if you like, you can run TeX in --verbose mode'
+
+# check for --quiet flag (yes, this is sloppy)
+function compile()
+{
+  if [ "$1" == '--verbose' ]
+  then
+    xelatex "$FNAME.tex"
+  else
+    xelatex -interaction=batchmode "$FNAME.tex"
+  fi
+}
+
+echo 'BUILD.SH: compiling for first time'
+compile $1
+echo 'BUILD.SH: recompiling for references'
+compile $1
+echo 'BUILD.SH: merging cover page with documentation body'
+#gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=RogueRobotics_RogueUnderwaterSolutions_TechnicalDocumentation_2019.pdf "$FNAME-cover.pdf" "$FNAME.pdf"
+echo 'BUILD.SH: done'
