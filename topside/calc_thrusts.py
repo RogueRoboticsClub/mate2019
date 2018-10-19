fb2e518ab6ed7780a918799b98ecc3fee0d17c93
Eric Zheng <zheng.eric.y@gmail.com>
2018-10-18 21:43:23 -0400

    write nice thrust solver interface

--- topside/calc_thrusts.py
+++ topside/calc_thrusts.py
@@ -5,7 +5,9 @@
 #
 # for theory and nomenclature, see 'docs/misc/dynamics/dynamics.pdf'
 
+# should we replace numpy with our own solving routines?
 import numpy as np
+
 
 
 # the ROV design paramters are described by the thruster configuration:
@@ -17,75 +19,83 @@ class Thruster(object):
         self.psi, self.alpha = orientation
 
 
-# filters a matrix for values greater in magnitude than the tolerance
-# alternatively, sets values lesser in magnitude than the tolerance to zero
-def zero(mat, tol=1e-10):
-    return np.where(abs(mat) > tol, mat, 0)
+
+# this is the primary interface for solving 3D ROV thrust problems
+# a usage example can be seen by running this file
+class Solver(object):
+    # compute the structure matrix from a list of Thruster objects
+    def setup(self, thruster_list):
+        self.thrusters = thruster_list
+        self.structure_matrix = self.__zero(np.array([[
+            np.sin(t.psi) * np.cos(t.alpha),
+            np.sin(t.psi) * np.sin(t.alpha),
+            np.cos(t.psi),
+            t.r*np.sin(t.theta)*np.sin(t.phi)*np.cos(t.psi) -
+                t.r*np.cos(t.theta)*np.sin(t.psi)*np.sin(t.alpha),
+            t.r*np.cos(t.theta)*np.sin(t.psi)*np.cos(t.alpha) -
+                t.r*np.sin(t.theta)*np.cos(t.phi)*np.cos(t.psi),
+            t.r*np.sin(t.theta)*np.sin(t.psi)*np.sin(t.alpha - t.phi)]
+        for t in self.thrusters]).transpose())
 
 
-# setup function computes the structure matrix B
-# it takes a list of Thruster objects
-def setup(thrusters):
-    B = np.array([[
-        np.sin(t.psi) * np.cos(t.alpha),
-        np.sin(t.psi) * np.sin(t.alpha),
-        np.cos(t.psi),
-        t.r*np.sin(t.theta)*np.sin(t.phi)*np.cos(t.psi) -
-            t.r*np.cos(t.theta)*np.sin(t.psi)*np.sin(t.alpha),
-        t.r*np.cos(t.theta)*np.sin(t.psi)*np.cos(t.alpha) -
-            t.r*np.sin(t.theta)*np.cos(t.phi)*np.cos(t.psi),
-        t.r*np.sin(t.theta)*np.sin(t.psi)*np.sin(t.alpha - t.phi)]
-    for t in thrusters]).transpose()
-    return zero(B)
+    # solve for the thrusts needed to achieve the given resultant
+    # optional: tolerance for failure checking (default 0.0001)
+    # returns:  vector of thrusts (for sign conventions, see documentation)
+    #           success (True if this is a solution, False if failed)
+    def solve(self, resultant, tol=0.0001):
+        # equivalent to a least squares regression to minimize residual norm
+        solution = self.__zero(
+            np.linalg.lstsq(self.structure_matrix, resultant, rcond=-1)[0])
+
+        # check to see if solution was a success (Bf ~ F)
+        if not np.count_nonzero(self.__zero(
+            np.matmul(self.structure_matrix, solution) - resultant, tol=tol)):
+            return solution, True
+        else:
+            return solution, False
 
 
-# solves for vector thrusts f
-# arguments: resultant F, structure matrix B
-# optional: tolerance for failure checking (default 0.0001)
-# returns: vector f (for sign conventions, see documentation)
-#          success (True if this is a solution, False if failed)
-def solve(F, B, tol=0.0001):
-    # equivalent to a least squares regression to minimize the residual norm
-    # we don't use the matrix inverse because not all B are invertible
-    solution = zero(np.linalg.lstsq(B, F, rcond=-1)[0])
+    # private ultility function
+    # zeros values in a matrix that are lesser in magnitude than tolerance
+    def __zero(self, mat, tol=1e-10):
+        return np.where(abs(mat) > tol, mat, 0)
 
-    # check to see if solution was a success (Bf ~ F)
-    if not np.count_nonzero(zero(np.matmul(B, solution) - F, tol=tol)):
-        return solution, True
+
+
+# handy usage example: run this file with the interpreter
+if __name__ == '__main__':
+    # for feasibility testing, we will try the DART ADAM ROV values
+    DART_r = 0.5          # estimate r = 0.5 m
+    DART_theta = np.pi/2  # unspecified, so assume everything is coplanar
+    DART_psi = 4*np.pi/9  # 80 deg = 4pi/9 rad
+
+    # setup the solver
+    ThrustSolver = Solver()
+    ThrustSolver.setup([
+        Thruster((DART_r, DART_theta,   np.pi/4), (DART_psi, 3*np.pi/4)),
+        Thruster((DART_r, DART_theta, 3*np.pi/4), (DART_psi,   np.pi/4)),
+        Thruster((DART_r, DART_theta, 5*np.pi/4), (DART_psi, 7*np.pi/4)),
+        Thruster((DART_r, DART_theta, 7*np.pi/4), (DART_psi, 5*np.pi/4)),
+        Thruster((DART_r/np.sqrt(2), DART_theta,     0), (0, 0)),
+        Thruster((DART_r/np.sqrt(2), DART_theta, np.pi), (0, 0))
+    ])
+
+    # now let's assume we want 1 N in the x-direction on the ROV
+    # we represent this as [fx, fy, fz, tx, ty, tz]
+    # for a force f along an axis and a torque t about an axis
+    objective = [1, 0, 0, 0, 0, 0]
+    thrusts, success = ThrustSolver.solve(objective)
+
+    # display results
+    print('The desired resultant is:')
+    print(objective)
+    print()
+    if success:
+        print('The algorithm converged with solution:')
+        print(thrusts)
     else:
-        return solution, False
-
-
-# for feasibility testing, try the DART ADAM ROV values
-DART_r = 1            # choose units so that r = 1
-DART_theta = np.pi/2  # documentation does not specify theta
-DART_psi = 4*np.pi/9  # 80 deg = 4pi/9 rad
-DART = zero(setup([
-    Thruster((DART_r, DART_theta,   np.pi/4), (DART_psi, 3*np.pi/4)),
-    Thruster((DART_r, DART_theta, 3*np.pi/4), (DART_psi,   np.pi/4)),
-    Thruster((DART_r, DART_theta, 5*np.pi/4), (DART_psi, 7*np.pi/4)),
-    Thruster((DART_r, DART_theta, 7*np.pi/4), (DART_psi, 5*np.pi/4)),
-    Thruster((DART_r/np.sqrt(2), DART_theta,     0), (0, 0)),
-    Thruster((DART_r/np.sqrt(2), DART_theta, np.pi), (0, 0))
-]))
-
-# TODO: investigate failures at [0,1,0,0,0,0] and [0,0,0,1,0,0]
-# TODO: investigate if this actually works
-objective = np.array([0, 0, 0, 0, 0, 1])
-solution, success = solve(objective, DART)
-
-print('The structure matrix is:')
-print(DART)
-print()
-print('The desired resultant is:')
-print(objective)
-print()
-if success:
-    print('The algorithm converged with solution:')
-    print(solution)
-else:
-    print('The algorithm failed to converge to a solution. Best attempt:')
-    print('  ', solution)
-    print('This yields a resultant of:')
-    print('  ', zero(np.matmul(DART, solution), tol=1e-8))
-    print('Check that the objective is feasible with the given design.')
+        print('The algorithm failed to find a solution. Best attempt:')
+        print('  ', solution)
+        print('This yields a resultant of:')
+        print('  ', zero(np.matmul(DART, solution), tol=1e-8))
+        print('Check that the objective is feasible with this design.')
